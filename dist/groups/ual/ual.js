2e484d92f1742a39225332d7459a80b68defae25
Christopher Green <cg433n@email.arizona.edu>
2017-08-21 15:31:38 -0700

    Adds ual settings

---
+++ dist/groups/ual/ual.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  // Remove links from profile images
+  $("img[src*='profiles']").unwrap();
+
+  // Replace profile box title with "Your librarian"
+  $("div[id*='profile_box'] > h2").contents().replaceWith("Your librarian");
+
+  // remove empty p tags inserted after hr
+  $("hr + p").filter( function() {
+    return $.trim($(this).html()) == ' ';
+  }).remove();
+
+  // Remove breadcrumb links
+  $('#s-lib-bc').remove();
+
+  // Hide side tabs if there is only one page and no in-page navigation
+  var navigation = $("ul.nav-pills.nav-stacked");
+  var listitems = navigation.children();
+  if (navigation.children("li").length <= 1 && listitems.children("ul").length == 0) {
+    navigation.hide();
+  }
+});
